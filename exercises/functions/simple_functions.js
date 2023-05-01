582c6f7c7fd867a31cf744dab86a09f718d318b5
William Roberson <billkeyshawn03@gmail.com>
2023-05-01 11:41:37 -0400

    WHy

---
+++ exercises/functions/simple_functions.js
@@ -0,0 +1,19 @@
+function favoriteThings(){
+    console.log("Will's favorite things are: ");
+    console.log("My favorite movie is Friday.");
+    console.log("My favorite band are the beetles.");
+}
+
+favoriteThings();
+
+function whyImHere() {
+    console.log(
+        `I really like the students and their attitudes 
+        and feel like I'm making a difference in their lives.`
+    );
+}
+
+whyImHere();
+
+
+
