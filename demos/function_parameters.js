d16f7795cff9a080fb8288430eb586fe626c62b0
William Roberson <billkeyshawn03@gmail.com>
2023-05-01 14:47:53 -0400

    parameter

---
+++ demos/function_parameters.js
@@ -0,0 +1,17 @@
+//function receives the parameter
+function print(message) {
+    console.log(message);
+}
+
+//call function pass arguments
+print("A given message");
+print("I'm getting hungry");
+print("when are we going to take lunch");
+
+let myMessage = "test this out";
+print(myMessage);
+
+
+
+
+
