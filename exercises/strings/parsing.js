3d60da12ef5872547907278bfbd6cac3daba85bc
William Roberson <billkeyshawn03@gmail.com>
2023-05-02 15:10:03 -0400

    let name

---
+++ exercises/strings/parsing.js
@@ -0,0 +1,10 @@
+let name = "William Roberson";
+
+let positionOfSpace = name.indexOf(" ");
+console.log(positionOfSpace);
+
+let first = name.substring(0, positionOfSpace);
+console.log(first);
+
+let last = name.substring(positionOfSpace + 1);
+console.log(last);  
