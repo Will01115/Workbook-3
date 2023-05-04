bee67d23578909d604c88e0c558ccc21fa5de631
William Roberson <billkeyshawn03@gmail.com>
2023-05-04 11:18:04 -0400

    Setting Date fields

---
+++ exercises/Dates/DateScripts/more_date_conversions.js
@@ -0,0 +1,9 @@
+
+let birthDate = new Date("02/14/2003");
+console.log(birthDate.toLocaleString());
+
+let yourMomsBirthday = new Date("AUG 09 1983");
+console.log(yourMomsBirthday.toLocaleString());
+
+let yourDadsBirthday = new Date ("1985, 09, 24");
+console.log(yourDadsBirthday.toLocaleString());
